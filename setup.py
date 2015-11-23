98a9843cb7c24e57bfccc2d183b16789094cb3ce
Jamil Atta Junior <atta.jamil@gmail.com>
2015-11-23 16:57:02 -0200

    Adicionando o setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="Opac Schema",
+    version='0.1',
+    description="Schema of SciELO OPAC"
+    author="SciELO",
+    author_email="scielo@scielo.org",
+    license="BSD",
+    url="https://github.com/scieloorg/opac_schema",
+    py_modules=['opac_schema'],
+    keywords='opac schema',
+    maintainer_email='scielo@scielo.org',
+    download_url='',
+    classifiers=[],
+    setup_requires=["mongoengine"],
+    test_suite='tests'
+)
