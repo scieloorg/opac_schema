5ae23a45c1a650ab426408cf049cb67ec6b69790
Juan Funez <juan.funez@gmail.com>
2016-04-06 10:44:29 -0300

    version BUMP! 1.8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup(
     name="Opac Schema",
-    version='1.7',
+    version='1.8',
     description="Schema of SciELO OPAC",
     author="SciELO",
     author_email="scielo@scielo.org",
