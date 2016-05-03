f92dd396f1e324e35fdf4640d11a7d87378deea1
Jamil Atta Junior <atta.jamil@gmail.com>
2016-05-03 16:24:47 -0300

    Versão 2.2 do opac_schema.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup(
     name="Opac Schema",
-    version='2.1',
+    version='2.2',
     description="Schema of SciELO OPAC",
     author="SciELO",
     author_email="scielo@scielo.org",
