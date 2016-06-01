422d2db72a42be87f0308610e6e305373fa23f4a
Jamil Atta Junior <atta.jamil@gmail.com>
2016-06-01 12:05:38 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup(
     name="Opac Schema",
-    version='2.6',
+    version='2.7',
     description="Schema of SciELO OPAC",
     author="SciELO",
     author_email="scielo@scielo.org",
