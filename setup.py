256cd486857dd041d12ba63b11b98d586474c26b
Jamil Atta Junior <atta.jamil@gmail.com>
2017-07-26 16:11:48 -0300

    Adicionando o doi e order na formação da url do artigo.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name="Opac Schema",
-    version='2.33',
+    version='2.34',
     description="Schema of SciELO OPAC",
     author="SciELO",
     author_email="scielo@scielo.org",
