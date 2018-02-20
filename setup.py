99ec8c2d066b8e030a6c437f13a4bb60e5276152
Juan Funez <juan.funez@gmail.com>
2018-02-20 16:47:48 -0300

    bump version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name="Opac Schema",
-    version='2.40',
+    version='2.41',
     description="Schema of SciELO OPAC",
     author="SciELO",
     author_email="scielo@scielo.org",
