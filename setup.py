a4d4e0e29bbf4f26dba7c6605c4c3e47df390bfb
Jamil Atta Junior <atta.jamil@gmail.com>
2018-05-15 14:39:17 -0300

    Adiciona flag enable_contact no Journal

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name="Opac Schema",
-    version='2.42',
+    version='2.43',
     description="Schema of SciELO OPAC",
     author="SciELO",
     author_email="scielo@scielo.org",
