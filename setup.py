568a73ca88f2ef3b0236c2282149d44303ed0086
Joffily Ferreira <joffily.ferreira@scielo.org>
2020-05-18 16:21:25 -0300

    Bump version 2.55

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name="Opac Schema",
-    version='2.54',
+    version='2.55',
     description="Schema of SciELO OPAC",
     author="SciELO",
     author_email="scielo@scielo.org",
